efdf07e73bf6fad0c8abada400b1904ef50bdcc5
me-abhijith-v <me.abhijith18@gmail.com>
2026-05-19 20:34:52 +0530

    shell

--- shell_pgms/simple_calc.sh
+++ shell_pgms/simple_calc.sh
@@ -1,29 +1,62 @@
-    #!/bin/bash
+#!/bin/bash
 
-    echo "enter first number: "
-    read a
+# Simple Menu Driven Calculator
 
-    echo "enter first number: "
-    read b
+while true
+do
+    echo "----------------------"
+    echo " MENU DRIVEN CALCULATOR "
+    echo "----------------------"
+    echo "1. Addition"
+    echo "2. Subtraction"
+    echo "3. Multiplication"
+    echo "4. Division"
+    echo "5. Exit"
+    echo "---------------------"
 
-    echo " enter operator "
-    read op
+    read -p "Enter your choice: " choice
 
-    if [ $op = "+" ]; then
-    result=$((a+b))
-    elif [ $op = "-" ]; then
-    result=$((a-b))
-    elif [ $op = "*" ]; then
-    result=$((a*b))
-    elif [ $op = "/" ]; then
-    result=$((a/b))
-    
-    elif [ $op = "%" ]; then
-    result=$((a%b))
+    if [ $choice -eq 5 ]
+    then
+        echo "Exiting..."
+        break
+    fi
 
-    fi 
-    echo "result is $result"
+    read -p "Enter first number: " a
+    read -p "Enter second number: " b
 
+    case $choice in
 
+        1)
+            result=$((a + b))
+            echo "Result = $result"
+            ;;
 
-    
+        2)
+            result=$((a - b))
+            echo "Result = $result"
+            ;;
+
+        3)
+            result=$((a * b))
+            echo "Result = $result"
+            ;;
+
+        4)
+            if [ $b -eq 0 ]
+            then
+                echo "Division by zero not possible"
+            else
+                result=$((a / b))
+                echo "Result = $result"
+            fi
+            ;;
+
+        *)
+            echo "Invalid Choice"
+            ;;
+
+    esac
+
+    echo
+done
